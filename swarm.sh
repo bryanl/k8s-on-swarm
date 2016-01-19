6c5bc03ba42b57c907d9bcfdf5014c568a32b81c
bryanl <bryan@osesm.com>
2016-01-19 15:14:43 -0500

    create cluster in a single region

--- swarm.sh
+++ swarm.sh
@@ -2,11 +2,7 @@
 
 set -e
 
-name="${2:-swarm}"
 
-if [[ $DEBUG_SWARM == "1" ]]; then
-  echo "debug mode"
-fi
 
 up() {
   export DIGITALOCEAN_ACCESS_TOKEN=$DO_PAT
@@ -14,58 +10,101 @@ up() {
   export DIGITALOCEAN_USERDATA='cloud-config.yml'
 
 
-  echo "booting swarm: $name"
+  echo "booting swarm: $name in $region"
 
   if [[ $(checkNode "$name-kvstore") == "1" ]]; then
-    echo "creating keystore"
-    docker-machine -D create -d digitalocean --digitalocean-region tor1 $name-kvstore
+    echo "creating keystore droplet"
+    docker-machine $debug_mode create -d digitalocean --digitalocean-region $region $name-kvstore 
     docker $(docker-machine config $name-kvstore) run -d --net=host progrium/consul --server -bootstrap-expect 1
   fi
 
   kvip=$(docker-machine ip $name-kvstore)
 
   if [[ $(checkNode "$name-master") == "1" ]]; then
-    echo "creating master"
-    docker-machine -D create -d digitalocean --digitalocean-region tor1 \
+    echo "creating master droplet"
+    docker-machine $debug_mode create -d digitalocean --digitalocean-region $region \
       --swarm --swarm-master --swarm-discovery consul://${kvip}:8500 \
       --engine-opt "cluster-store consul://${kvip}:8500" \
-      --engine-opt "cluster-advertise eth0:2376" $name-master
+      --engine-opt "cluster-advertise eth0:2376" $name-master 
+
+    if [[ $? != 0 ]]; then
+      echo "couldn't create master"
+      exit 1
+    fi
   fi
 
-  for region in nyc1 nyc2 nyc3 tor1 sfo1; do
-    for i in {1..3}; do 
-      node=$(printf "%s-%s-%02d" $name $region $i)
+  for i in $(seq 1 $node_count); do 
+    node=$(printf "%s-%s-%02d" $name $region $i)
 
-      if [[ $(checkNode "$node") != "1" ]]; then
-        echo "skipping $node"
-        continue
-      fi
+    if [[ $(checkNode "$node") != "1" ]]; then
+      echo "skipping $node"
+      continue
+    fi
 
-      echo "creating node: $node"
-      docker-machine -D create -d digitalocean --digitalocean-region $region \
-        --swarm --swarm-discovery consul://${kvip}:8500 \
-        --engine-label region=$region
-        --engine-opt "cluster-store consul://${kvip}:8500" \
-        --engine-opt "cluster-advertise eth0:2376" $node
-    done
+    echo "creating droplet node: $node"
+    docker-machine $debug_mode create -d digitalocean --digitalocean-region $region \
+      --swarm --swarm-discovery consul://${kvip}:8500 \
+      --engine-label region=$region \
+      --engine-opt "cluster-store consul://${kvip}:8500" \
+      --engine-opt "cluster-advertise eth0:2376" $node &
   done
+
+  wait
 
   eval $(docker-machine env --swarm $name-master)
   docker network ls
 }
 
 checkNode() {
-  docker-machine status $1 > /dev/null
+  docker-machine status $1 &> /dev/null
   echo $?
 }
 
 down() {
-  docker-machine ls | grep $name | awk '{print $1}' | xargs docker-machine rm -f
+  echo "removing cluster $name"
+  docker-machine ls | awk '{print $1}' | grep $name | xargs docker-machine rm -f
 }
 
-case $1 in
+usage() {
+  echo "usage: $0 [-c <node count>] [-d] [-n <cluster name>] [-r <region>]"
+  exit 0
+}
+
+# defaults
+name="swarm"
+region="nyc1"
+node_count=3
+
+while getopts ":c:dn:r:" opt; do
+  case "${opt}" in
+    c)
+      node_count=${OPTARG}
+      ;;
+
+    d)
+      debug_mode="-D"
+      ;;
+
+    n)
+      name=${OPTARG}
+      ;;
+
+    r)
+      region=${OPTARG}
+      ;;
+
+    *)
+      usage
+      ;;
+  esac
+done
+
+shift $((OPTIND-1))
+action="$1"; shift
+
+case $action in
   up)
-    up
+    up $@
     ;;
   down)
     down
